a6032a2b7e5864330bb51be15ee81325279a9d5f
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-03-16 16:19:13 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry
- * return always 0
+ * main - a program that prints a line with puts function
+ * return: 0 (success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	Puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
