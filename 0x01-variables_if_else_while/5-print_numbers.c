4798d842611e6a23446b2f032f0593692b104a44
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-03-20 16:16:54 -0700

    d

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * a program to print the first ten numbers from 0 to 9
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		printf("%d\n", a);
+	return (0);
+}
