f0a0cfc483a488090132826acbab4add46bf5d3a
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-03-21 14:19:41 -0700

    c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "main.h"
 /**
  * main - Entry point
@@ -17,4 +18,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+	return (0);
 }
