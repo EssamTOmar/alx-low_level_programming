455a896368b9db114edefbfd6176159a7ea88a87
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-03-20 18:16:14 -0700

    c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * combinr numbers
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	for (b = '0'; b <= '9'; b++)
+		if (a == b)
+		{
+			continue;
+		}
+	putchar(b);
+	putchar(';');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
