4593f7ca81e060413a1e76776836966377c786d2
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-03-23 12:40:07 -0700

    s

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiblies two numbers
+ * @a: first number to be multiblied
+ * @b: second number to be multiblied
+ * Return: the multiblication final result
+ */
+int mul(int a, int b)
+{
+	printf("%d\n", mul(a, b));
+}
