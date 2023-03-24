0239490c090371ac7ffae37916693143e8f53516
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-03-24 03:57:52 -0700

    d

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
- * more_numbers - print the numbers from 0 to 14 
+ * more_numbers - print the numbers from 0 to 14
  * ten times in new lines
  *
  * Return: Always 0.
  */
 void more_numbers(void)
 {
-	int(num);
-	int a =0;
+	int num;
+	int a = 0;
 
-	while (a <= '14')
+	while (a <= 14)
 	{
-		for ( num = '0'; num <= '14'; num++ )
-		_putchar(num);
+		for (num = 0; num <= 14; num++)
+		{
+			_putchar(num);
+		}
 		_putchar('\n');
+		a++;
 	}
 	_putchar('\n');
-	return (0);
 }
