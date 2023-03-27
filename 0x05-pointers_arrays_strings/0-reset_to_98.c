63c270834f211d40c6204ef1c2e3b2f795352750
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-03-27 11:17:25 -0700

    t

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,9 @@
 #include "main.h"
 /**
  * reset_to_98 - assingning a value of 98 to variable
- * @*n: the address of variable that we will assign to
- *
- * return - the value of n
+ * @n: the address of variable that we will assign to
  */
 void reset_to_98(int *n)
 {
-	int c = 1;
-	int p = &c;
-
-	reset_to_98(*p);
+	*n = 98;
 }
