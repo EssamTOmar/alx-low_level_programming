984a920a9e873300a3f7984daa959bcddcf49964
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-03-27 13:08:45 -0700

    r

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _puts - a function that prints a string, followed by a new line
+ * @str: the string to be printed
+ * Return: string to the standard output
+ */
+void _puts(char *str)
+{
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+}
