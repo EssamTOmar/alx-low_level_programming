76d82a72b0b346b900494eec3e2d1845dd917a29
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-03-27 13:33:54 -0700

    r

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - a function that prints a string, in reverse
+ * @s: the string to be brinted in reverse
+ * Return: the sting in reverse
+ */
+void print_rev(char *s)
+{
+	int n;
+
+	for (s[n] = '\0'; n <= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+}
