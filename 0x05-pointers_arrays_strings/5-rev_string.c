e9698759cf8c7626967fc3afcc40f55fc3e4204e
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-03-27 15:17:52 -0700

    r

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * rev_string - a function that reverses a string
+ * @s: the string to be reversed
+ * Return: the reversed string
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int i;
+	char *begin_pointer;
+	char *end_pointer;
+	char temp;
+
+	begin_pointer = s;
+	end_pointer = s;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	len--;
+	for (i = 0; i <= len ; i++)
+	{
+		end_pointer++;
+	}
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = *end_pointer;
+		*end_pointer = *begin_pointer;
+		*begin_pointer = temp;
+		begin_pointer++;
+		end_pointer--;
+	}
+}
