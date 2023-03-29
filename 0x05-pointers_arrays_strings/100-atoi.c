b9791dae237218879a729d67b5cd323976ae0681
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-03-28 18:11:05 -0700

    r

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _atoi - a function that convert a string to an integer
  * @s: the string to be converted
@@ -8,5 +9,6 @@ int _atoi(char *s)
 {
 	int x = _atoi(s);
 
+	printf("%d\n", x);
 	return (0);
 }
