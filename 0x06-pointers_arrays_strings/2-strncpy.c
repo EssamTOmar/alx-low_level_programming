5661a73237787a1ddbcbc597dd3978250bf2139d
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-03-30 14:34:04 -0700

    r

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,8 +12,9 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
+		if (src[i] != '\0')
+		{
+			dest[i] = src[i];
+		}
 	return (dest);
 }
