f25d04e0e1b863883cb8f2bfdce3061cb242d413
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-03-30 12:16:18 -0700

    r

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: the char to be converted
+ *
+ * Return: the string's characters all in uppercase
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (str[i] >= 'a'; str[i] <= 'z'; i++)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
