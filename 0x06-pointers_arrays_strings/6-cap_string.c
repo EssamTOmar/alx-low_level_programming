175958fcbf86d2ddf6b1a31538fcc6892ed25e49
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-03-30 13:53:16 -0700

    e

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * cap_string - a function that capitalizes all words of a string
+ * @str: the string to be capitalized
+ *
+ * Return: capitalized string
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+	}
+	return (str);
+}
