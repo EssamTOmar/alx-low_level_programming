9c1aa98dca9ef8e0122edbdca2710ec535b26557
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-04-10 06:49:07 -0700

    r

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * @argc: the count of argumments
+ * @argv: the array of pointers
+ * Return: always 0if (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
