8e46d0f1b554e1ffe73a88a3c55e935a59eb95c4
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-04-10 07:57:57 -0700

    r

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,23 +4,23 @@
  * main - Entry Point
  * @argc: the count of arguments
  * @argv: array og pointers
- * Return: 0 if succes 1 if error
+ * Return: 0 (success) 1 (error)
  */
 int main(int argc, char *argv[])
 {
-	int mul;
-	int num1;
-	int num2;
+	int result, num1, num2;
 
 	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
-	mul = num1 * num2;
-	printf("%d\n", mul);
+	result = num1 * num2;
+
+	printf("%d\n", result);
 
 	return (0);
 }
