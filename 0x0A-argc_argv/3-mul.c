f958bdcee35c0bcf6ea3de4334b2f84bfcd26345
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-04-10 07:53:07 -0700

    e

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,13 +9,17 @@
 int main(int argc, char *argv[])
 {
 	int mul;
+	int num1;
+	int num2;
 
 	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	mul = _atoi(argv[1]) * _atoi(argv[2]);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	mul = num1 * num2;
 	printf("%d\n", mul);
 
 	return (0);
