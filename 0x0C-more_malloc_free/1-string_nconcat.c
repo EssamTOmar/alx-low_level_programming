34bda8bc1df3e1456c50482ad3d647f808c83b9d
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-04-13 16:48:22 -0700

    s

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,10 +32,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	if (n >= len2)
-	{
-		snew = malloc(len1 + len2 + 1);
-	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		snew[i] = s1[i];
@@ -43,6 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (m = 0; m < n; m++)
 	{
 		snew[i] = s2[m];
+		i++;
 	}
 	snew[i] = '\0';
 	return (snew);
