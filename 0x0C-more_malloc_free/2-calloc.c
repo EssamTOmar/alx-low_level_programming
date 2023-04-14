6d3a818cfd82fb69fc3d9332c2079c33566035bb
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-04-13 17:04:44 -0700

    e

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,8 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *a;
+	int *a;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -19,6 +20,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (a == 0)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+		a[i] = 0;
 	}
 	return (a);
 }
