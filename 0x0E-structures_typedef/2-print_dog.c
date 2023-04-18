06b9c2488c7912ac049e153a986a3930f4778590
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-04-18 14:34:51 -0700

    e

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,15 +9,15 @@ void print_dog(struct dog *d)
 {
 	if (d->name == NULL)
 	{
-		printf("Name: %s\nAge: %f\nOwner: %s\n", "(nil)", d->age, d->owner);
+		printf("Name: %s\n","(nil)");
 	}
 	if (d->age < 0)
 	{
-		printf("Name: %s\nAge: %s\nOwner: %s\n", d->name, "(nil)", d->owner);
+		printf("Age: %s\n", "(nil)");
 	}
 	if (d->owner == NULL)
 	{
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, "(nil)");
+		printf("Owner: %s\n", "(nil)");
 	}
 	if (d == NULL)
 	{
