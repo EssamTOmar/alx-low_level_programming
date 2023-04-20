c08b9d2f2365d728819e65b3728e0a0e8ba81d53
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-04-20 15:38:19 -0700

    v

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,19 +9,20 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	va_start(args, n);
 
 	int sum = 0;
+
+	va_start(args, n);
 
 	if (n == 0)
 	{
 		return (0);
 	}
-	for (int i = 0; i < n; i++)
+	for (unsigned int i = 0; i < n; i++)
 	{
 		sum += va_arg(args, unsigned int);
 	}
-	va_end (args);
+	va_end(args);
 
 	return (sum);
 }
