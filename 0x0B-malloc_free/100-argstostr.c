1668b4ac2e710bea632fe03c23731fe7e87df6e9
EssamTOmar <essam.taha@pharma.asu.edu.eg>
2023-04-29 15:36:07 -0700

    r

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,50 +1,47 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-/**
- * argstostr - function that concatenates all the arguments of your program
- * @ac: number of arguments
- * @av: the arguments to be concatenated
- * Return: pointer to a new string, or NULL if it fails
- */
-char *argstostr(int ac, char **av)
-{
-	char *str;
-	int i, row, col, size;
 
-	if (ac == 0 || av == NULL)
-	{
-		return (NULL);
-	}
-	col = 0;
-	size = 0;
-	for (row = 0; row < ac; row++)
-	{
-		while (av[row][col])
-		{
-			size++;
-			col++;
-		}
-		size++;
-	}
-	str = malloc((sizeof(char) * size) + 1);
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	i = 0;
-	col = 0;
-	for (row = 0; row < ac; row++)
-	{
-		while (av[row][col])
-		{
-			str[i] = av[row][col];
-			i++;
-			col++;
-		}
-		str[i] = '\n';
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ *
+ * Return: 1 if it is, 0 it's not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
